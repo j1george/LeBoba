38ba26a13b0e35ae79d5c28ca434c63680b9c2bd
George Jone <g.george.jone@gmail.com>
2019-04-20 13:55:04 -0700

    create templates for data objects

---
+++ data_templates.js
@@ -0,0 +1,15 @@
+const Order = {
+    id: 0,
+    isComplete: False,
+    items: [],
+    customerEmail: ""
+};
+
+const Item = {
+    name: ""
+}
+
+const Customer = {
+    email: "",
+    orders: []
+}
